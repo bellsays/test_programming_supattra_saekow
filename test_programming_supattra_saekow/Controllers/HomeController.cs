ab6fdb7a6567c85deb04bf7222ddd02c4a9bd01d
Supattra Saekow <64847261+bellsays@users.noreply.github.com>
2023-11-02 00:19:00 +0700

    Add project files.

---
+++ test_programming_supattra_saekow/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿//using AspNetCore;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using test_programming_supattra_saekow.Models;
+
+namespace test_programming_supattra_saekow.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Q1()
+        {
+           return View();
+        }
+
+        public IActionResult Q2()
+        {
+            return View();
+        }
+        public IActionResult Q3()
+        {
+            return View();
+        }
+        public IActionResult Q4()
+        {
+            return View();
+        }
+        public IActionResult Q5()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ test_programming_supattra_saekow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test_programming_supattra_saekow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
